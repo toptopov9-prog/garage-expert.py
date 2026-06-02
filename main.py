171ba1385022f4692cdeee44d6e802a7a7abb814
toptopov9-prog <toptopov9@gmail.com>
2026-06-02 14:49:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 def read_root():
     return {"status": "Мост Гаражного Эксперта запущен и готов к работе!"}
 
-@app.post("/api/alice")
+@app.post("/api/алиса")
 async def alice_handler(request: dict):
     req_data = request.get("request", {})
     command = req_data.get("command", "")
